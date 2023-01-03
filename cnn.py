3c784e9c56f345e0952e8d311332f319acae6b38
Akanksha Dubey <91489416+akankshadubey-202@users.noreply.github.com>
2023-01-03 20:10:31 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,43 +1,46 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-heart=pd.read_csv("heart.csv")
+dataset=pd.read_csv("heart_data.csv")
 
-
-X=heart.iloc[:,0:13]
-y=heart.iloc[:,13:14]
+X=dataset.iloc[:,0:13]
+y=dataset.iloc[:,13:14]
 
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=42)
 
 ss=StandardScaler()
 X_train=ss.fit_transform(X_train)
 X_test=ss.fit_transform(X_test)
 
-import tensorflow as tf
+
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Dropout
-from keras import models
+
 #Creating a pipeline
-model = models.Sequential()
+model = Sequential()
+
 #1st hidden layer with input layer
-model.add(Dense(units=145,activation="relu",input_dim=13))
+model.add(Dense(units=170,activation="relu",input_dim=13))
+
 #2nd hidden layer
 model.add(Dense(units=120,activation="relu",))
+
 #3rd hidden layer
 model.add(Dense(units=70,activation="relu",))
-#output layer
-#model.add(Dense(units=1,activation="sigmoid"))
-model.add(Dense(1, activation=tf.nn.sigmoid))
 
-#model Summary
+
+
+#output layer
+model.add(Dense(units=1,activation="sigmoid"))
+
 model.summary()
 model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
-model_his=model.fit(X_train,y_train,validation_split=0.1, batch_size=55,epochs=50,verbose=1)
+model_his=model.fit(X_train,y_train,validation_split=0.30, batch_size=55,epochs=25,verbose=1)
 
 
 y_pred=model.predict(X_test)
-y_pred = (y_pred > 0.5)
+y_pred = (y_pred > 0.45)
 
 
 from sklearn.metrics import accuracy_score
@@ -50,7 +53,6 @@ print(cm)
 
 
 print(classification_report(y_test,y_pred))
-
 
 import matplotlib.pyplot as plt
 # summarize history for accuracy
@@ -71,8 +73,5 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
-
-model.save('heart.h5',model_his)
-
-
-print("model created")
+model.save("heart_model.h5")
+print("Model Created")
